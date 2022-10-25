e28e57e7524d701935bdc5d22add1ab2de4a5fd4
samwhitford11 <samantha.fichte98@gmail.com>
2022-10-24 21:06:55 -0600

    made on click event and tried adding code to render data to screen

--- app.js
+++ app.js
@@ -14,12 +14,41 @@ function eventSearch(performer){
     .then((event) => {
         console.log(event)
 
+        
         //render the data
+        const $main = $("main")
+        $main.empty()
 
+        $main.html(`
+       
+        `)
+
+        
     })
     .catch((error)=>{
         console.log(error)
     })
 }
+$("input[type=submit]").on("click", (event) => {
+    //prevent for refresh
+    event.preventDefault()
+
+    //grab text from input box
+    const inputText = $("input[type=text]").val()
+
+    //update the screen
+    weatherSearch(inputText)
+})
 eventSearch("charlie-puth")
+
+
+// how to create a function that take the input 
+// arrow callback function for on submit event targeting the form element
+// select the form element and store in a variable 
+// chain a .on() jquery event
+// select the form input element 
+// store the form input value in a variable 
+// invoke our eventSearch() function passing the form input value as an argument
+
+
 
